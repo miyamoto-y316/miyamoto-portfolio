d39c9b5f856fc4a10d3e6b88d9387509c4ec2682
MiyamotoYusei <mukiryoku316@gmail.com>
2026-06-03 21:16:32 +0900

    refactor: `SectionSkill.tsx` のデータを配列にして、map関数で表示する形式に変更

--- components/templates/SectionSkill.tsx
+++ components/templates/SectionSkill.tsx
@@ -3,45 +3,49 @@
 import { Grid } from "@mui/material";
 import CardSkill from "@/components/molecules/CardSkill";
 
+const skills = [
+  {
+    color: "#F7DF1E",
+    label: "2",
+    labelTextColor: "black" as const,
+    iconSrc:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+    skillTitle: "JavaScript",
+  },
+  {
+    color: "#1677C7",
+    label: "2",
+    labelTextColor: "white" as const,
+    iconSrc:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+    skillTitle: "TypeScript",
+  },
+  {
+    color: "#53C1DE",
+    label: "1",
+    labelTextColor: "white" as const,
+    iconSrc:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    skillTitle: "React",
+  },
+  {
+    color: "#41B883",
+    label: "1",
+    labelTextColor: "white" as const,
+    iconSrc:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nuxtjs/nuxtjs-original.svg",
+    skillTitle: "Nuxt.js",
+  },
+];
+
 export default function Skill() {
   return (
     <Grid container spacing={4}>
-      <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-        <CardSkill
-          color="#F7DF1E"
-          label="2"
-          labelTextColor="black"
-          iconSrc="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
-          skillTitle="JavaScript"
-        />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-        <CardSkill
-          color="#1677C7"
-          label="2"
-          labelTextColor="white"
-          iconSrc="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
-          skillTitle="TypeScript"
-        />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-        <CardSkill
-          color="#53C1DE"
-          label="1"
-          labelTextColor="white"
-          iconSrc="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
-          skillTitle="React"
-        />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-        <CardSkill
-          color="#41B883"
-          label="1"
-          labelTextColor="white"
-          iconSrc="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nuxtjs/nuxtjs-original.svg"
-          skillTitle="Nuxt.js"
-        />
-      </Grid>
+      {skills.map((skill) => (
+        <Grid key={skill.skillTitle} size={{ xs: 12, sm: 6, md: 3 }}>
+          <CardSkill {...skill} />
+        </Grid>
+      ))}
     </Grid>
   );
 }
