4d3eba067385be46ba24c3eb894a93dd1aa9128b
MiyamotoYusei <mukiryoku316@gmail.com>
2026-06-03 21:07:40 +0900

    change: `CardWork.tsx` に受賞履歴のリボンを付与

--- components/molecules/CardWork.tsx
+++ components/molecules/CardWork.tsx
@@ -24,17 +24,59 @@ export default function CardWork({
       }}
     >
       <Box
-        component="img"
-        src={workSrc}
-        alt={srcAlt}
         sx={{
+          position: "relative",
           padding: 3,
-          width: "100%",
-          height: 400,
-          objectFit: "cover",
-          borderRadius: 10,
         }}
-      />
+      >
+        <Box
+          component="img"
+          src={workSrc}
+          alt={srcAlt}
+          sx={{
+            width: "100%",
+            height: 400,
+            objectFit: "cover",
+            borderRadius: 10,
+            display: "block",
+          }}
+        />
+        {/* リボンの影 */}
+        <Box
+          sx={{
+            position: "absolute",
+            top: 53,
+            right: 0,
+            background: "rgba(0, 0, 0, 0.3)",
+            fontWeight: "bold",
+            pl: 4,
+            pr: 2,
+            lineHeight: "30px",
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%, 20px 50%)",
+          }}
+        >
+          U22プログラミングコンテスト受賞
+        </Box>
+        {/* リボン本体 */}
+        <Box
+          sx={{
+            position: "absolute",
+            top: 48,
+            right: 0,
+            background:
+              "linear-gradient(135deg, #ffde7c 0%, #fcf049 50%, #daa923 100%)",
+            color: "#000",
+            fontWeight: "bold",
+            pl: 4,
+            pr: 2,
+            py: 0.5,
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%, 20px 50%)",
+            textShadow: "1px 1px 0px rgba(255, 255, 255, 1)",
+          }}
+        >
+          U22プログラミングコンテスト受賞
+        </Box>
+      </Box>
       <Typography sx={{ color: "#000000" }} variant="h5">
         {workTitle}
       </Typography>
